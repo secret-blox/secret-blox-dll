724beeaf3279652df6bdbe5f172fbb77a8b5be6c
uniquadev <noreply>
2024-02-19 15:13:35 +0100

    preparation luau macro shuffling

---
+++ Rbx/include/Luau/shuffle.hpp
@@ -0,0 +1,18 @@
+
+// just for reference
+#define LUAVM_SHUFFLE2(sep,a0,a1) a1 sep a0
+#define LUAVM_SHUFFLE3(sep,a0,a1,a2) a1 sep a2 sep a0
+#define LUAVM_SHUFFLE4(sep,a0,a1,a2,a3) a3 sep a1 sep a0 sep a2
+#define LUAVM_SHUFFLE5(sep,a0,a1,a2,a3,a4) a4 sep a0 sep a2 sep a1 sep a3
+#define LUAVM_SHUFFLE6(sep,a0,a1,a2,a3,a4,a5) a3 sep a5 sep a2 sep a0 sep a1 sep a4
+#define LUAVM_SHUFFLE7(sep,a0,a1,a2,a3,a4,a5,a6) a2 sep a3 sep a0 sep a4 sep a6 sep a1 sep a5
+#define LUAVM_SHUFFLE8(sep,a0,a1,a2,a3,a4,a5,a6,a7) a7 sep a0 sep a5 sep a6 sep a3 sep a1 sep a2 sep a4
+#define LUAVM_SHUFFLE9(sep,a0,a1,a2,a3,a4,a5,a6,a7,a8) a2 sep a6 sep a4 sep a7 sep a1 sep a8 sep a0 sep a3 sep a5
+
+/*
+* Example usage from 2016 source:
+* LUAVM_SHUFFLE2(;,
+*  unsigned short nCcalls,
+*  unsigned short baseCcalls
+* );
+*/
