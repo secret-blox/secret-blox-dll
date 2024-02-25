06eaad5929f85a5ebede45b750e4af9e5874ea37
uniquadev <58218460+uniquadev@users.noreply.github.com>
2024-02-25 17:07:30 +0100

    made some fixups

--- Internal/src/execution.cpp
+++ Internal/src/execution.cpp
@@ -4,10 +4,11 @@
 lua_State* SB::Execution::mainState = nullptr;
 uintptr_t __fastcall pseudo2addrHook(lua_State* L, int idx)
 {
+    // unhook function
+    // ...
     // create new thread from the current state
     SB::Execution::mainState = lua_newthread(L);
     lua_pop(L, 1); // clean stack
-    // unhook function
     // return normal pseudo2addr
     return 0; // TODO
 }
