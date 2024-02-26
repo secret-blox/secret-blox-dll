628ba6ec806ea69306b947396b08df96fb0946a8
uniquadev <58218460+uniquadev@users.noreply.github.com>
2024-02-26 15:57:13 +0100

    fixed shuffles and vmvalue

--- Luau/VM/include/lsecurity.hpp
+++ Luau/VM/include/lsecurity.hpp
@@ -1,18 +1,13 @@
-/*
-* Example usage from 2016 source:
-* LUAVM_SHUFFLE2(;,
-*  unsigned short nCcalls,
-*  unsigned short baseCcalls
-* );
-*/
+#pragma once
+#include <cstdint>
+#pragma warning(disable: 4003) // not enough actual parameters for macro 'identifier' warning
 
-// just for reference
 #define LUAVM_COMMA_SEP ,
 #define LUAVM_SEMICOLON_SEP ;
 #define LUAVM_SHUFFLE2(sep,a0,a1) a1 sep a0
 #define LUAVM_SHUFFLE3(sep,a0,a1,a2) a1 sep a2 sep a0
 #define LUAVM_SHUFFLE4(sep,a0,a1,a2,a3) a3 sep a1 sep a0 sep a2
-#define LUAVM_SHUFFLE5(sep,a0,a1,a2,a3,a4) a4 sep a0 sep a2 sep a1 sep a3
+#define LUAVM_SHUFFLE5(sep,a0,a1,a2,a3,a4) a0 sep a1 sep a3 sep a2 sep a4
 #define LUAVM_SHUFFLE6(sep,a0,a1,a2,a3,a4,a5) a3 sep a5 sep a2 sep a0 sep a1 sep a4
 #define LUAVM_SHUFFLE7(sep,a0,a1,a2,a3,a4,a5,a6) a2 sep a3 sep a0 sep a4 sep a6 sep a1 sep a5
 #define LUAVM_SHUFFLE8(sep,a0,a1,a2,a3,a4,a5,a6,a7) a7 sep a0 sep a5 sep a6 sep a3 sep a1 sep a2 sep a4
@@ -22,79 +17,47 @@
 * PtrObf implementation
 * author: uniquadev
 */
-#define COMMON_PTROBF_OPERATORS using RBX_PTROBF<T>::operator=; using RBX_PTROBF<T>::operator->; using RBX_PTROBF<T>::operator const T
-template <typename T> class RBX_PTROBF
+#define DEFINE_VMVALUE(name, obfBlock, deobfBlock) \
+    template <typename T> class RBX_VMVALUE_##name \
+    { \
+    public: \
+        operator const T() const deobfBlock \
+        const T operator->() { \
+            return operator const T(); \
+        } \
+        void operator=(const T& value) obfBlock \
+    protected: \
+        T storage; \
+    }; \
+
+DEFINE_VMVALUE(ADD,
 {
-public:
-    operator const T() const {
-        return deobf();
-    }
-    const T operator->() {
-        return deobf();
-    }
-
-    void operator=(const T& value) {
-        storage = obf(value);
-    }
-
-    virtual T deobf() const = 0;
-    virtual T obf(const T& value) const  = 0;
-protected:
-    T storage;
-};
-
-template <typename T> class RBX_PTROBF_ADD : RBX_PTROBF<T>
+    storage = (T)((uintptr_t)value - (uintptr_t)this);
+},
 {
-public:
-    COMMON_PTROBF_OPERATORS;
+    return (T)((uintptr_t)this->storage + (uintptr_t)this);
+});
 
-    T deobf() const override {
-        return (T)((uintptr_t)this->storage + (uintptr_t)this);
-    }
-
-    T obf(const T& value) const override {
-        return (T)((uintptr_t)value - (uintptr_t)this);
-    }
-};
-
-template <typename T> class RBX_PTROBF_SUB_X_P : RBX_PTROBF<T>
+DEFINE_VMVALUE(SUB_X_P,
 {
-public:
-    COMMON_PTROBF_OPERATORS;
-
-    T deobf() const override {
-        return (T)((uintptr_t)this->storage - (uintptr_t)this);
-    }
-
-    T obf(const T& value) const override {
-        return (T)((uintptr_t)this + (uintptr_t)value);
-    }
-};
-
-template <typename T> class RBX_PTROBF_SUB_P_X : RBX_PTROBF<T>
+    storage = (T)((uintptr_t)this + (uintptr_t)value);
+},
 {
-public:
-    COMMON_PTROBF_OPERATORS;
+    return (T)((uintptr_t)this->storage - (uintptr_t)this);
+});
 
-    T deobf() const override {
-        return (T)((uintptr_t)this - (uintptr_t)this->storage);
-    }
-
-    T obf(const T& value) const override {
-        return (T)((uintptr_t)this - (uintptr_t)value);
-    }
-};
-
-template <typename T> class RBX_PTROBF_XOR : RBX_PTROBF<T>
+DEFINE_VMVALUE(SUB_P_X,
 {
-public:
-    COMMON_PTROBF_OPERATORS;
+    storage = (T)((uintptr_t)this - (uintptr_t)value);
+},
+{
+    return (T)((uintptr_t)this - (uintptr_t)this->storage);
+});
 
-    T deobf() const override {
-        return (T)((uintptr_t)this->storage ^ (uintptr_t)this);
-    }
-
-    T obf(const T& value) const override {
-        return (T)((uintptr_t)value ^ (uintptr_t)this);
-    }
-};
+DEFINE_VMVALUE(XOR,
+{
+    storage = (T)((uintptr_t)value ^ (uintptr_t)this);
+},
+{
+    return (T)((uintptr_t)this->storage ^ (uintptr_t)this);
+});
