be56856e8269c31acfadd68d9991b981ba8ed5b7
uniquadev <58218460+uniquadev@users.noreply.github.com>
2024-02-27 20:24:42 +0100

    bytecodeencoder by joe

--- Internal/include/Internal/execution.hpp
+++ Internal/include/Internal/execution.hpp
@@ -1,5 +1,23 @@
 #include "lua.h"
+#include "Luau/Compiler.h"
+#include "Luau/BytecodeUtils.h"
+#include "Luau/BytecodeBuilder.h"
 
+class RbxBytecodeEncoder : public Luau::BytecodeEncoder {
+    inline void encode(uint32_t* data, size_t count) override {
+        // Loop through the instructions.
+        for (auto i = 0u; i < count;) {
+            // Get the opcode (which is the first byte of the instruction).
+            auto& opcode = *reinterpret_cast<uint8_t*>(data + i);
+
+            // Add the instruction length (which could be multiple 32-bit integers).
+            i += Luau::getOpLength(LuauOpcode(opcode));
+
+            // Encode the opcode.
+            opcode *= 227;
+        }
+    }
+};
 
 namespace SB::Execution {
     extern lua_State* mainState;
